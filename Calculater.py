4042d03a179c1d74b298bf999db2107b4655d32e
Dhruba Dey <dhrubadey2006@gmail.com>
2025-07-01 12:44:47 +0530

    Update Calculater.py

--- Calculater.py
+++ Calculater.py
@@ -1,11 +1,12 @@
 flag = "y" 
-while flag=="Y" or flag=="y":
-    a=int(input("enter the 1st number ="))
-    b=int(input("enter the 2nd number ="))
-    print('1. Addition')
-    print('2. Multiplication')
-    print('3. Division')
-    print('4. Subtraction')
+while  (flag=="Y" or flag=="y"):
+    a=int(input("enter the 1st number = "))
+    b=int(input("enter the 2nd number = "))
+    print('1. Addition(+)')
+    print('2. Multiplication(x)')
+    print('3. Division(/)')
+    print('4. Subtraction(-)')
+    print('5. Persentage(%)')
     chose = int(input("chose ="))
     if chose == 1:             
         print (a+b)
@@ -15,9 +16,11 @@ while flag=="Y" or flag=="y":
         print (a/b)
     elif chose == 4:
         print(a-b)
+    elif chose == 5:
+        print(a/b*100)
     else:
      print("invalid oparation chose")
-    flag=(input("do you want to continue(y/n):"))
+    flag=(input("Do you want to continue(y/n):"))
 while not ( flag == "N" or flag == "n"):
-     print("Invalid input! Please enter Y or N.")
-     flag = input("Do you want to continue? (Y/N): ")
+     print("Invalid input! Please enter y or n.")
+     flag = input("Do you want to continue? (y/n): ")
